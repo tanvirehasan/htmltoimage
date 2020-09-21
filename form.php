5fb5c310d2a8d702b0102cbb3ccfb3cc5074f458
tanvirehasan <tanvirhasanbcse@gmail.com>
2020-09-22 05:10:01 +0600

    Html to Image js code

---
+++ form.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</head>
+<body>
+
+	<style type="text/css">
+		.fimage{
+			margin-top: 25px;
+			margin-bottom: 25px;
+		}
+
+		h3{text-align: center;}
+	</style>
+
+
+<div class="col-md-6 mx-auto" style="margin-bottom: 50px;" >
+
+	<div class="col-md-5 mx-auto">
+		<img class="fimage" src="http://speakerbook.org/logo.png" width="300px">
+	</div>
+	<h3>WELCOME TO THE SPEAKERBOOK EXPERT NETWORK!</h3>
+	<p>SpeakerBook connects you to the largest network of thought leaders, speakers, celebrities, trainers and learning & development experts in Bangladesh and abroad across industries and themes. We want you to lead successfully, communicate effectively, manage professionally and to do much more...</p>
+	<hr>
+
+<form>
+	<div class="form-row">
+	    <div class="form-group col-md-6">
+	      <label >First name</label>
+	      <input type="text" class="form-control" placeholder="First name">
+	    </div>
+
+	    <div class="form-group col-md-6">
+	      <label >Last name</label>
+	      <input type="text" class="form-control" placeholder="Last name">
+	    </div>
+	  </div>
+
+	<div class="form-row">
+	  <div class="form-group col-md-6">
+	    <label >Email</label>
+	      <input type="email" class="form-control" placeholder="Enter Primary Email.">
+	    </div>
+	    <div class="form-group col-md-6">
+	     <label for="inputEmail4">Email</label>
+	      <input type="email" class="form-control" placeholder="Enter Secondary Email">
+	    </div>
+	</div>
+
+	<div class="form-row">
+	  <div class="form-group col-md-6">
+	    <label >Country</label>
+	      <input type="text" class="form-control" placeholder="Enter Country">
+	    </div>
+	    <div class="form-group col-md-6">
+	     <label >City</label>
+	      <input type="text" class="form-control" placeholder="Enter City">
+	    </div>
+	</div>
+
+	<div class="form-row">
+	  <div class="form-group col-md-6">
+	    <label >Mobile Number</label>
+	      <input type="text" class="form-control" placeholder="Enter Mobile Number">
+	    </div>
+	    <div class="form-group col-md-6">
+	     <label >LinkedIn/ Website Link </label>
+	      <input type="text" class="form-control" placeholder="Enter Link">
+	    </div>
+	</div>
+
+	<div class="form-row">
+	  <div class="form-group col-md-6">
+	    <label >Company</label>
+	      <input type="text" class="form-control" placeholder="Enter Company">
+	    </div>
+	    <div class="form-group col-md-6">
+	     <label>Designation</label>
+	      <input type="text" class="form-control" placeholder="Enter Designation">
+	    </div>
+	</div>
+
+	<div class="form-row">
+	  <div class="form-group col-md-6">
+	    <label>Gender</label>    
+	    <select class="custom-select" required>
+	    	<option>Male</option>
+	    	<option>Female</option>
+	    </select>       
+	    </div>
+	   <div class="form-group col-md-6">
+	   		<label>Upload photo</label>
+	   		<input type="file" class="form-control"  name="">	    
+		</div>
+	</div>
+
+
+	<div class="form-row">
+	  <div class="form-group col-md-6">
+	    <label>Industry</label>    
+	    <select class="custom-select" required>
+	    	<option>Male</option>
+	    	<option>Female</option>
+	    </select>       
+	    </div>
+
+	  <div class="form-group col-md-6">
+	    <label>Functional Area</label>    
+	    <select class="custom-select" required>
+	    	<option>select</option>
+	    	<option>Corporate</option>
+	    	<option>Nonprofit</option>
+	    	<option>government organizations</option>
+	    </select>         
+	  </div>
+	</div>
+
+	<div class="form-row">
+	<div class="form-group col-md-6">
+	    <label >Topics of Expertise</label>
+	      <input type="text" class="form-control" placeholder="Topics of Expertise">
+	    </div>
+	</div>
+		<div class="form-group col-md-6">
+			<label >Commercial Expectation</label>
+		      <input type="text" class="form-control" placeholder="Ex: 1000$, 50000 taka .. etc">
+		</div>
+
+
+	<button type="button" class="btn btn-success btn-block">Submit</button>
+
+</form>
+</div>
+
+</body>
+</html>
+
